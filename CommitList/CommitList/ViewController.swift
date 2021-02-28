36ef2d488be5f000098ccb10bf974502b39cf3dc
Tristan Newman <tristanb.newman@gmail.com>
2021-02-28 08:03:35 -0600

    project creation, initial commit

---
+++ CommitList/CommitList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommitList
+//
+//  Created by Tristan Newman on 2/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
